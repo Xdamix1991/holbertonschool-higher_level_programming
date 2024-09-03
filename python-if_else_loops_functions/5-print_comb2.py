f4a9869a152d4aa23281a550b8d4e4b22b25284c
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-03 16:39:01 +0200

    5-print_comb2.py

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    #print til 9 int to digit expl"01"
+    if i < 10:
+        print(f"0{i}",end=', ' if i < 9 else'')
+
+    else:
+        print(i, end=', ' if i < 99 else '')
+print()
+
