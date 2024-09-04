74d3cf65dabaec81791826b0944a610f081dfdc5
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-04 16:16:35 +0200

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments():
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(number))
+
+    for count in range(1, len(sys.argv)):
+        print("{}: {}".format(count, sys.argv[count]))
+
+
+if __name__ == "__main__":
+    print_arguments()
