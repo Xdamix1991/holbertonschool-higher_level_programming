0057614929920f68e3ced9d751b3ca731f6e0d28
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-04 16:20:48 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ def print_arguments():
     if number == 0:
         print("0 arguments.")
     elif number == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(number))
 
