1cc96bcb612476ea1c823d16e4e7c48f7e663402
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-04 09:27:16 +0200

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,6 @@
 
 for i in range(0, 100):
     if i == 99:
-        print("{}".format(i))
-# print til 9 int to digit expl"01"
+        print("{}".format(i), end='\n')
     else:
         print("{:02}".format(i), end=", ")
