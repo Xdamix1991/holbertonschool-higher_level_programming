4d39cb4ce5526b3c99b63c0ad5b8a378f6e3a7c9
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-04 15:15:17 +0200

    corrected opperators

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ b = 5
 addition = add(a, b)
 print("{} + {} = {}".format(a, b, addition))
 substraction = sub(a, b)
-print("{} + {} = {}".format(a, b, substraction))
+print("{} - {} = {}".format(a, b, substraction))
 multiplication = mul(a, b)
-print("{} + {} = {}".format(a, b, multiplication))
+print("{} * {} = {}".format(a, b, multiplication))
 division = div(a, b)
-print("{} + {} = {}".format(a, b, division))
+print("{} / {} = {}".format(a, b, division))
 if __name__ == "__main__":
     pass
