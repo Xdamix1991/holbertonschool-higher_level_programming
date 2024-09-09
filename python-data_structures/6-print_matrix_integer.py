4832a363d06b0678155d73609016a94a6c9e5288
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-09 12:58:31 +0200

    added a print if no argument passed to the function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix or not any(matrix):
-        return
+        print()
     for i in matrix:
         if not i:
             continue
