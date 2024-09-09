a78def681569abb805008499a6490f48f4e663ca
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-09 12:18:52 +0200

    added if not matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix or not any(matrix):
+        return
     for i in matrix:
+        if not i:
+            continue
         for j in i:
-            print("{}".format(j), end=' ')
+            if j == i[0]:
+                print("{:d}".format(j), end='')
+            else:
+                print(" {:d}".format(j), end='')
         print()
