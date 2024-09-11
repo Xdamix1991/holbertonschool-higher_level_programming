c34cc5bb8b070833b02012f8b07cd40e955ee3a6
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-11 09:26:20 +0200

    removed handlinf IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except IndexError:
-            print("list index out of range")
         except(TypeError, ValueError):
-            pass
+            continue
     print()
     return (count)
