9a3dcdd30c515a08bd07dd6d20f83181eb3676ed
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-11 10:21:26 +0200

    removed () around return

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if not matrix or not any(matrix):
-        return (None)
+        return None
     result = []
+
     for i in matrix:
         result.append([j ** 2 for j in i])
-    return (result)
+    return result
