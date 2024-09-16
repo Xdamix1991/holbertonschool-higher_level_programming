58a704b7f0e584267cb8e140f369a171cd266c58
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-16 10:14:27 +0200

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains classe of a square
+ """
+
+
+class Square:
+    """
+     This class represents a square
+     type: is a public class attribute
+       """
+    type = None
