6c118c1a97a2cf852e5f62fd67a7a28361840007
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-16 10:28:08 +0200

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains classe of a square
+ """
+
+
+class Square:
+    """
+     This class represents a square
+     type: is a public class attribute
+       """
+    def __init__(self, size):
+        """
+        function to initialize a square size
+           """
+        self.__size = size
