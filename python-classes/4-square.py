60848c75c68d3a5f25dc2dbafc7f030e6921feaf
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-16 12:00:17 +0200

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains classe of a square
+ """
+
+
+class Square:
+    """
+     This class represents a square
+     size: is a private instance attribut
+       """
+    def __init__(self, size=0):
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        calculates the area of the square
+           """
+        return self.__size ** 2
