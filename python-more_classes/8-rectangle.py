987c37d0014f83ea1537ad79c1820e83ec5c7e9f
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-17 10:48:15 +0200

    8-rectangle.py

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+in this module we create a class called rectangular
+ """
+
+
+class Rectangle:
+    """
+    This class defines an rectangle.
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        area = self.width * self.height
+        return area
+
+    def perimeter(self):
+        perimeter = (self.width + self.height) * 2
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            return perimeter
+
+    def __str__(self):
+        rectangle_print = ""
+
+        if self.width == 0 or self.height == 0:
+            return ""
+        for i in range(self.height):
+            rectangle_print += str(self.print_symbol) * self.width
+            if i < self.height - 1:
+                rectangle_print += "\n"
+        return rectangle_print
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_2.area() > rect_1.area():
+            return rect_2
+        else:
+            return rect_1
