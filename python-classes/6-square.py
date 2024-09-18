7f1c45c2ad199ed8785c27fb2a46bffe451161a7
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-18 10:39:12 +0200

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for _ in range(self.__postion[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                print(" " * self.__postion[0] + "#" * self.__size)
