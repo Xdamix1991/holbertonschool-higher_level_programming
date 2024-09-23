491592ccec2c7c9a9d7eae2df6d702094ed3145f
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-23 14:59:16 +0200

    corrected booleen condition

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,11 +13,13 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
+
+
 
 
 class Rectangle(BaseGeometry):
@@ -27,6 +29,6 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
