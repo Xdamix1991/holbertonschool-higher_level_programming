4d8441c28e578f30e1fb08e3d8636db33fd17b0d
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-23 15:53:43 +0200

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+this mosule contains a class named BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle that heritate from Basegeometry
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        printing = str("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return printing
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        printing_sq = str("[Square] {}/{}".format(self.__size, self.__size))
+        return printing_sq
