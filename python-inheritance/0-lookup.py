eebe91e9d29c6b029b167c2f9a5c1fc6ccfe1074
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-23 11:05:46 +0200

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains a module to iterate and return what in an object
+"""
+
+
+def lookup(obj):
+    """
+function to look up on attributr and methodes on an object
+"""
+    return dir(obj)
