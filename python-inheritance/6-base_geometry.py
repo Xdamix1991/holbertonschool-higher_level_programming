29588b3dccfe4df470aa07b1505086f6948fd6fe
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-23 13:06:05 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this mosule contains a class named BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
