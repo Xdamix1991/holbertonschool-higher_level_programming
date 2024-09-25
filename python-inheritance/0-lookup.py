7aae5947505f5ef47e63afc11b269f0c9f3394c2
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-25 08:33:54 +0200

    corrected syntax

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,7 +6,9 @@ contains a module to iterate and return what in an object
 
 def lookup(obj):
     """
-function to look up on attributr and methodes on an object
-"""
+    function to look up on attributr and methodes on an object
+    returns:
+        list
+    """
 
     return dir(obj)
