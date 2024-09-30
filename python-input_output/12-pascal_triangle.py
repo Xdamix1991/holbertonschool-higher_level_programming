b19bc16e6cb02999317ea7b9444b3c1d026b94ca
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-30 18:25:52 +0200

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+in this module function that generate pascal triangle
+ """
+import math
+
+
+def pascal_triangle(n):
+    """
+    n the size of the triangle
+    """
+
+    liste = []
+
+    for i in range(n):
+        liste.append([1] * (i + 1))
+
+        for j in range(1, i):
+            liste[i][j] = liste[i - 1][j - 1] + liste[i - 1][j]
+    return liste
