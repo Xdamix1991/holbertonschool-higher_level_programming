d520e90c1ae73ad6bb938bb07d82917e32f9013a
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-30 14:41:41 +0200

    correct code to manage double list created

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,10 +8,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv
+args = sys.argv[1:]
 filename = "add_item.json"
-with open(filename, 'w', encoding="utf-8"):
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
     my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+
+my_list.extend(args)
+save_to_json_file(my_list, filename)
