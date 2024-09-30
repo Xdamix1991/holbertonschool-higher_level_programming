e8dc8b7e214d622d5d5e9f72553d438c17131b33
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-30 12:30:26 +0200

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+in this mosule a function writes an Object to a text file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    my_obj:
+        object to convert
+    filename:
+        to put the object into
+    """
+    objects = json.dumps(my_obj)
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(objects)
+        return objects
