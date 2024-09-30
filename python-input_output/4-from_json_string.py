255c94e75df8f9ac5c69bf55a99f0645d2d323ed
Xdamix1991 <bourichalamine1@gmail.com>
2024-09-30 11:50:24 +0200

    added documentation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -6,5 +6,12 @@ import json
 
 
 def from_json_string(my_str):
+    """
+    my_str:
+      the string json to convert
+    objecs:
+        returned as abject type
+    """
+
     objecs = json.loads(my_str)
     return objecs
