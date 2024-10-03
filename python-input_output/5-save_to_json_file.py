80c5f3a4efdbe4b54c420d73c6020ed8c611d0f2
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-03 13:09:06 +0200

    corrected verion

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,9 +13,5 @@ def save_to_json_file(my_obj, filename):
         to put the object into
     """
 
-    objects = json.dumps(my_obj)
-
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(objects)
-
-    return objects
+        json.dump(my_obj, f)
