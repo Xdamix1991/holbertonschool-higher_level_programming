d8e3af2738bc2013d9b0c557758fad4c01c1f3e1
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-09 14:38:35 +0200

    removed encoding

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,7 +8,7 @@ import http.server
 import json
 
 
-class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+class Run(http.server.BaseHTTPRequestHandler):
     """
     this class a simple server
     """
@@ -24,19 +24,12 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("Hello, this is a simple API!".encode())
 
-        elif self.path == '/info':
-            data_info = {"version": "1.0",
-                         "description": "A simple API built with http.server"}
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(json.dumps(data_info).encode())
-
         elif self.path == '/data':
-            dataset = {"name": "John", "age": 30, "city": "New York"}
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(dataset).encode())
+            self.wfile.write(json.dumps(data).encode())
 
         elif self.path == '/status':
             self.send_response(200)
@@ -54,5 +47,5 @@ if __name__ == "__main__":
     PORT = 8000
     server_adress = ('', PORT)
     server = http.server.HTTPServer
-    httpd = server(server_adress, SimpleAPIHandler)
+    httpd = server(server_adress, Run)
     httpd.serve_forever()
