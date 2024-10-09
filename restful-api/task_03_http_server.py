94a9eb180d13834bc9dc9ad86763d22d797a0000
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-09 14:16:27 +0200

    removed encoding

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -20,21 +20,21 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
+            self.wfile.write("Hello, this is a simple API!".encode())
 
         elif self.path == '/info':
             data_info = {"version": "1.0",
                          "description": "A simple API built with http.server"}
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(json.dumps(data_info).encode('utf-8'))
+            self.wfile.write(json.dumps(data_info).encode())
 
         elif self.path == '/data':
             dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(dataset).encode('utf-8'))
+            self.wfile.write(json.dumps(dataset).encode())
 
         elif self.path == '/status':
             self.send_response(200)
@@ -45,7 +45,7 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write("404 Not Found".encode('utf-8'))
+            self.wfile.write("404 Not Found".encode())
 
 
 if __name__ == "__main__":
