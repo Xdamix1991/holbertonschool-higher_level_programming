2499491aef67c3fa0d974cc42e324b1c1ff2d474
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-09 13:57:06 +0200

    corrected syntax

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -19,25 +19,27 @@ class Run(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
 
         elif self.path == '/info':
-            self.send_response(200)
-            self.end_headers()
             data_info = {"version": "1.0",
                          "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.end_headers()
             self.wfile.write(json.dumps(data_info).encode('utf-8'))
 
         elif self.path == '/data':
+            dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            dataset = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(dataset).encode('utf-8'))
 
         elif self.path == '/status':
             self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write("OK".encode('utf-8'))
         else:
             self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write("404 Not Found".encode('utf-8'))
 
