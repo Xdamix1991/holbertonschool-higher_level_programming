ca5516a60782e4aa05d4e5fbbc9d0fef81da5406
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-14 15:00:29 +0200

    corrected users list

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -5,18 +5,22 @@ this module contains sample api to authetify users and admins
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt
-import secrets
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager
+from flask_jwt_extended import get_jwt
 
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
-app.config['JWT_SECRET_KEY'] = secrets.token_hex(32)
-app.config['SECRET_KEY'] = secrets.token_hex(32)
+app.config['JWT_SECRET_KEY'] = 'key-secret'
 
-users = {}
+users = {
+    "user1": {"password": generate_password_hash("1234"),
+              "role": "user"},
+    "admin1": {"password": generate_password_hash("adminpass"),
+               "role": "admin"}
+}
 
 
 @auth.verify_password
@@ -25,7 +29,7 @@ def verify_password(username, password):
     if user and check_password_hash(user['password'], password):
         return username
     else:
-        return "Unauthorized", 401
+        return None
 
 
 @app.route('/basic-protected')
@@ -43,25 +47,25 @@ def login():
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
         access_token = create_access_token(identity=username, additional_claims={"role": user['role']})
-        return jsonify(access_token=access_token), 200
+        return jsonify({"access_token": access_token}), 200
     else:
-        return "user not found", 401
+        return jsonify({"error": "User not found"}), 401
 
 
 @app.route('/jwt-protected')
 @jwt_required()
 def protected():
-    return "JWT Auth: Access Granted"
+    return jsonify({"JWT Auth: Access Granted"})
 
 
 @app.route('/admin-only')
 @jwt_required()
 def admin():
     admin_role = get_jwt()
-    if admin_role['role'] == 'admin':
-        return "Admin Access: Granted"
+    if admin_role.get('role') == 'admin':
+        return jsonify({"Admin Access: Granted"})
     else:
-        return "access denied", 403
+        return jsonify({"error": "Admin access required"}), 403
 
 
 if __name__ == "__main__":
