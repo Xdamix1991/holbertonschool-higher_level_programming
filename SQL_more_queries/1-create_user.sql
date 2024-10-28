e387e1eed28554f7fcd665725591f21ab448e197
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-28 11:58:27 +0100

    corrected syntaxe

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS "user_0d_1"@"mylocalhost" IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* to "user_0d_1"@"mylocalhost";
+-- script that creates the MySQL server user user_0d_1 and grant all previligies to it
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* to "user_0d_1"@"localhost";
