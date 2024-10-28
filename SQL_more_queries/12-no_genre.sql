b9979e1e7b96d0f021d9ddd1db391a0ce26b6023
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-28 15:42:40 +0100

    corrected left join with NULL values only

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows contained in the database hbtn_0d_tvshows NULL matched only
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
