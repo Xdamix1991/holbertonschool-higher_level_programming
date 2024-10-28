a5a568b4b192e6a96ccbbbd812abb353e81be652
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-28 14:05:17 +0100

    forgoted uppercases

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 from cities, states
-where state_id.cities = states.id
+WHERE state_id.cities = states.id
 ORDER BY cities.id ASC;
