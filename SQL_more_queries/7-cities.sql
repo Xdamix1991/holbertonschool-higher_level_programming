be8ec17ee95f701bcf8f32fa193cad745375a264
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-28 13:02:55 +0100

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE If NOT EXISTS cities(
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	state_id INT NOT NULL,
 	name VARCHAR(256),
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
