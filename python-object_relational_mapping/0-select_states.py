f0c237fb4db54304dc2ee04e1e43fb4b7912e75d
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-29 13:01:18 +0100

    pycodestyle checkerd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,18 @@ import MySQLdb
 from sys import argv
 
 
-conn = MySQLdb.connect(host="localhost",
-                       port=3306,
-                       user=argv[1],
-                       passwd=argv[2],
-                       db=argv[3],
-                       charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
