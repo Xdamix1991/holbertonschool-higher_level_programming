8e04b7a81269b272bc8c7f342579f19266ecf3c0
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-30 15:02:10 +0100

    10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that prints the State object with the name passed as argument
+ from the database hbtn_0e_6_usa
+ """
+
+from sqlalchemy import func
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    query = session.query(State).filter(
+        State.name == sys.argv[4]).order_by(State.id).all()
+
+    if query:
+        print("{}".format(query.id))
+    else:
+        print("Not found")
+
+    session.close()
