8b8ee1a8185715b37fb84fa853694a9b9c8fb3e6
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-30 11:29:51 +0100

    added documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,11 +10,13 @@ Base = declarative_base()
 
 
 class State(Base):
-
+    """
+    class state , inherating from Base
+    """
     __tablename__ = "states"
 
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
 
     def __repr__(self):
-        return "<State(name='%s')>"%(self.name)
+        return "<State(name='%s')>" % (self.name)
