22158101a84b629233210a66413bf9537bcb521c
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-30 09:56:25 +0100

    pycodestyle corrected

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                            charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states"
+    cur.execute("SELECT * FROM states "
                 "WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
 
