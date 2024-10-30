90673e11ad1de136d5c6928ee3d9e388ddb4c8f9
Xdamix1991 <bourichalamine1@gmail.com>
2024-10-30 16:12:41 +0100

    corrected output

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     session = Session()
 
     query = session.query(State).filter(
-        State.name.like('%a')).order_by(State.id)
+        State.name.like('%a%')).order_by(State.id)
     rows = query.all()
     for row in rows:
         print("{}: {}".format(row.id, row.name))
