820590b18c9329a3d826dc587d3eef0472afe481
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-08 12:23:37 +0100

    task_00_intro.py

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-
 """
 Module contenant une fonction pour générer des invitations avec des templates.
 """
-
 import re
+
 
 def generate_invitations(template, attendees):
 
@@ -15,16 +13,14 @@ def generate_invitations(template, attendees):
     if not attendees or not isinstance(attendees, list):
         raise TypeError("attendees must be a list")
 
-
     placeholders = re.findall(r'\{(.*?)\}', template)
 
     for index, attendee in enumerate(attendees):
         result_str = template
         for placeholder in placeholders:
             value = attendee.get(placeholder, "N/A")
-            result_str = result_str.replace(f'{{{placeholder}}}', str(value) if value else "N/A")
+            result_str = result_str.replace(f'{{{placeholder}}}',
+                                            str(value) if value else "N/A")
 
         with open(f'output_{index}.txt', 'w') as file:
             file.write(result_str)
-
-
